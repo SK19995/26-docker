49f29e26ba0ad5f682cbce81535209b553a7c27e
root <root@localhost.localdomain>
2020-06-02 02:52:29 -0700

    增加了打jar包

---
+++ dockerrun.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+mvn clean package
+cd target
+echo 'FROM docker.io/openjdk:8-jdk-buster
+MAINTAINER sk
+ADD demo-0.0.1-SNAPSHOT.jar demo.jar
+EXPOSE 9090
+ENTRYPOINT ["java","-jar","demo.jar"]' > Dockerfile
+sleep 5 
+docker build -t my/demo .
+sleep 5
+docker run -itd -p 9090:9090 my/demo:latest
+
